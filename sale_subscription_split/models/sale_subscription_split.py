5b0fde798e48007dcc3b3d3924677c5c872af079
FABRIZIO D'ADAMO <118846249+FabrizioDadamoStesi@users.noreply.github.com>
2022-12-04 20:00:52 +0100

    Add files via upload

--- sale_subscription_split/models/sale_subscription_split.py
+++ sale_subscription_split/models/sale_subscription_split.py
@@ -2,9 +2,14 @@ from odoo import fields, models
 
 class SaleOrder(models.Model):
     _inherit = 'product.template'
-    #_name = 'sale.order'
-    #_description = 'Sale subscription lines'
+    product_id = fields.Many2one('product.product', string='Product')
+    period = fields.Char(string='every')
+    months = fields.Selection([('1month', '1 Month'), ('2months','2 Months'), ('3months', '3 Months'),('6months','6 Months'),('1year', '1 Year')],string='Cadence')
+    qty = fields.Integer(string='Quantity')
 
+
+
+    #linea codice per richiamare le linee di ordine nel tab Delivery Note
     subscription_line_ids= fields.One2many('delivery.id.lines','delivery_id', string='Products Lines')
 
 class DeliveryIdLines(models.Model):
